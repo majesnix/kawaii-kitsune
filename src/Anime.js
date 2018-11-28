850be77aa26ef09fcfda8e0c47adec7a612d1176
majesnix <dominic@dominic-classen.de>
2018-11-28 06:09:02 +0100

    fix posterImage ts error (i think)

--- src/Anime.js
+++ src/Anime.js
@@ -1,43 +1,51 @@
 class Anime {
-	constructor(data) {
-		this.id = data.id;
-		this.type = data.type;
-		this.slug = data.attributes.slug;
-		this.synopsis = data.attributes.synopsis;
-		this.titles = {
-			english: data.attributes.titles.en,
-			romaji: data.attributes.titles.en_jp,
-			japanese: data.attributes.titles.ja_jp,
-			canonical: data.attributes.canonicalTitle,
-			abbreviated: data.attributes.abbreviatedTitles
-		};
-		this.averageRating = data.attributes.averageRating;
-		this.ratingFrequencies = data.attributes.ratingFrequencies;
-		this.userCount = data.attributes.userCount;
-		this.favoritesCount = data.attributes.favoritesCount;
-		this.startDate = data.attributes.startDate;
-		this.endDate = data.attributes.endDate;
-		this.popularityRank = data.attributes.popularityRank;
-		this.ratingRank = data.attributes.ratingRank;
-		this.ageRating = data.attributes.ageRating;
-		this.ageRatingGuide = data.attributes.ageRatingGuide;
-		this.subType = data.attributes.subtype;
-		this.posterImage = data.attributes.posterImage;
-		this.coverImage = data.attributes.coverImage;
-		this.episodeCount = data.attributes.episodeCount;
-		this.episodeLength = data.attributes.episodeLength;
-		this.youtubeVideoId = data.attributes.youtubeVideoId;
-		this.showType = data.attributes.showType;
-		this.nsfw = data.attributes.nsfw;
-	}
+  constructor(data) {
+    this.id = data.id;
+    this.type = data.type;
+    this.slug = data.attributes.slug;
+    this.synopsis = data.attributes.synopsis;
+    this.titles = {
+      english: data.attributes.titles.en,
+      romaji: data.attributes.titles.en_jp,
+      japanese: data.attributes.titles.ja_jp,
+      canonical: data.attributes.canonicalTitle,
+      abbreviated: data.attributes.abbreviatedTitles
+    };
+    this.averageRating = data.attributes.averageRating;
+    this.ratingFrequencies = data.attributes.ratingFrequencies;
+    this.userCount = data.attributes.userCount;
+    this.favoritesCount = data.attributes.favoritesCount;
+    this.startDate = data.attributes.startDate;
+    this.endDate = data.attributes.endDate;
+    this.popularityRank = data.attributes.popularityRank;
+    this.ratingRank = data.attributes.ratingRank;
+    this.ageRating = data.attributes.ageRating;
+    this.ageRatingGuide = data.attributes.ageRatingGuide;
+    this.subType = data.attributes.subtype;
+    this.posterImage = {
+      tiny: data.attributes.posterImage.tiny,
+      small: data.attributes.posterImage.small,
+      large: data.attributes.posterImage.large,
+      original: data.attributes.posterImage.original,
+      meta: data.attributes.posterImage.meta
+    };
+    this.coverImage = data.attributes.coverImage;
+    this.episodeCount = data.attributes.episodeCount;
+    this.episodeLength = data.attributes.episodeLength;
+    this.youtubeVideoId = data.attributes.youtubeVideoId;
+    this.showType = data.attributes.showType;
+    this.nsfw = data.attributes.nsfw;
+  }
 
-	get url() {
-		return `https://kitsu.io/anime/${this.id}/`;
-	}
+  get url() {
+    return `https://kitsu.io/anime/${this.id}/`;
+  }
 
-	get youtubeURL() {
-		return this.youtubeVideoId ? `https://www.youtube.com/watch?v=${this.youtubeVideoId}` : null;
-	}
+  get youtubeURL() {
+    return this.youtubeVideoId
+      ? `https://www.youtube.com/watch?v=${this.youtubeVideoId}`
+      : null;
+  }
 }
 
 module.exports = Anime;
